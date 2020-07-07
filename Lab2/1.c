6b548abde2ff60b2b5e28f1dc3f33a763194ea89
1834902578 <67970382+1834902578@users.noreply.github.com>
2020-07-08 01:59:52 +0600

    Create 1.c

---
+++ Lab2/1.c
@@ -0,0 +1,21 @@
+/*Insert any value at tenth index of the array,and print the whole array.*/
+
+#include<stdio.h>
+ int main(){
+     int arr[11]={1,2,3,4,5,6,7,8,9,10,11};
+
+     printf("Original Array : \n");
+
+     int i;
+     for(i=0;i<11;i++){
+          printf("%d ",arr[i]);
+        }
+        array[10] = 123 ;
+
+     printf("\nAfter INSERT value: \n ");
+     for(i=0;i<11;i++){
+          printf("%d ",arr[i]);
+        }
+
+     return 0 ;
+ }
