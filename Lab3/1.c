1b1605872cbf8623cc80095fabcfa177203e7dd4
1834902578 <67970382+1834902578@users.noreply.github.com>
2020-07-08 02:20:01 +0600

    Create 1.c

---
+++ Lab3/1.c
@@ -0,0 +1,22 @@
+*Find maximum from the array*/
+#include<stdio.h>
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int i, max;
+
+     printf(" Array :\n");
+     for(i=0;i<5;i++){
+         printf("%d ",arr[i]);
+     }
+     max = arr[0];
+
+     for(i=1;i<5;i++){
+         if(max<arr[i]){
+             
+             max=arr[i];
+         }
+     }
+     printf("\nMinimum value : %d  ",max);
+
+    return 0;
+}
