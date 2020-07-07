9f66453d3621419cb1ff1f0853ee0cf3468447e8
1834902578 <67970382+1834902578@users.noreply.github.com>
2020-07-08 02:11:38 +0600

    Create 9.c

---
+++ Lab2/9.c
@@ -0,0 +1,44 @@
+*Delete a value from kth indexfrom the array shift all other value to left .And print the whole array*/
+
+#include<stdio.h>
+int main()
+{
+    int arr_size = 5;
+    int i,index,j;
+    int array[arr_size];
+
+    printf("Please Enter Array Element : \n");
+    for(i=0;i<arr_size;i++)
+    {
+        scanf("%d ",&array[i]);
+    }
+
+    printf("Before deleting : \n");
+
+    for(i=0;i<arr_size;i++)
+    {
+        printf("%d ",array[i]);
+    }
+
+    printf("\nPlease enter deleting index (0-4) : ");
+    scanf("%d",&index);
+
+
+    j=index;
+
+    while(j<arr_size-1)
+    {
+      array[j]=array[j+1];
+        j++;
+
+    }
+    arr_size = arr_size-1;
+
+    printf("\nAfter deleting : ");
+    for(i=0;i<arr_size;i++)
+    {
+        printf("%d ",array[i]);
+    }
+
+    return 0;
+}
